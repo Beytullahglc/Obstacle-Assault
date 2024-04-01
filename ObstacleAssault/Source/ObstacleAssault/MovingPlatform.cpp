18624661c572a692819995a6777dd34c92386c61
Beytullah GÜLAÇ <Beytullahglc@users.noreply.github.com>
2024-04-01 18:21:54 +0300

    Engelden kaçış oyunu
    C++ ile geliştirilen engelden kaçış oyunu

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Move platform forward
+		// Get current location
+		FVector CurrentLocation = GetActorLocation();
+		// Add vector to that locatin
+		CurrentLocation.X = CurrentLocation.X + 1;
+		// Set the location
+		SetActorLocation(CurrentLocation);
+	// Send platform back if gone too far
+		// Check how far we have moved
+		// Reverse direction of motion if gone too far
+}
+
